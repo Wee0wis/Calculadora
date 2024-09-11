8aedb71a6b3a4a99b293be96553ac9c8acfb1cc1
Wee0wis <elgorditopulido25@gmail.com>
2024-09-10 22:51:28 -0700

    Primer commit

---
+++ src/styles/Colors.ts
@@ -0,0 +1,11 @@
+export const myColors = {
+    light: '#008080',
+    dark: '#17171C',
+    blue: '#CD5C5C',
+    btnGray: '#8E505F',
+    btnDark: '#2E2F38',
+    gray: '#FFFFFF',
+    black: '#000000',
+    white: '#FFFFFF', 
+    result: '#FFFFFF',
+}
